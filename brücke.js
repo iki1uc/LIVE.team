134491456aad54c1a39b19c6e3248837b269dd4e
iki1uc <entertrainer@web.de>
2026-08-23 19:56:32 +0200

    Update brücke.js

--- brücke.js
+++ brücke.js
@@ -1,8 +1,34 @@
+// brücke.js
+
 const FAIL_NEUTRAL = require("./fail.neutral.js");
 
-function BRUECKE(signal) {
-  if (!signal) {
-    return FAIL_NEUTRAL("Signal fehlt");
-  }
-  return signal;
+// QI = Erkenntnis (Rolle, Ort, Home)
+function QI(rolle, ort, home) {
+    return rolle + ort + home;
 }
+
+// IQQ = Weis (Suite, Bildung)
+function IQQ(suite, bildung) {
+    return suite + bildung;
+}
+
+// Continuum = Zugang durch Qualität
+function continuumZugang(qi, iqq) {
+    try {
+        const summe = qi + iqq;
+
+        if (summe > 0) {
+            return "Continuum geöffnet";
+        } else {
+            return "Continuum geschlossen";
+        }
+    } catch (error) {
+        return FAIL_NEUTRAL(error);
+    }
+}
+
+module.exports = {
+    QI,
+    IQQ,
+    continuumZugang
+};

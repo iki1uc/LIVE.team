1663a044b7764aabdd7d69d2531a2f2deacfd14f
iki1uc <entertrainer@web.de>
2026-08-23 19:53:37 +0200

    Update brücke.js

--- brücke.js
+++ brücke.js
@@ -1,33 +1,8 @@
-// brücke.js
+const FAIL_NEUTRAL = require("./fail.neutral.js");
 
-// QI = Erkenntnis (Rolle, Ort, Home)
-// IQQ = Weis (Suite, Bildung)
-// Continuum = Zugang durch Qualität
-
-function QI(rolle, ort, home) {
-    // QI↑ = Summe der Tatbestands-Qualität
-    return rolle + ort + home;
+function BRUECKE(signal) {
+  if (!signal) {
+    return FAIL_NEUTRAL("Signal fehlt");
+  }
+  return signal;
 }
-
-function IQQ(suite, bildung) {
-    // IQQ↑ = Summe der Vorgangs-Qualität
-    return suite + bildung;
-}
-
-function continuumZugang(qi, iqq) {
-    const summe = qi + iqq;
-
-    // VECTOR entscheidet den Zugang
-    if (summe > 0) {
-        return "Continuum geöffnet";
-    } else {
-        return "Continuum geschlossen";
-    }
-}
-
-// Export für dein Repo
-module.exports = {
-    QI,
-    IQQ,
-    continuumZugang
-};

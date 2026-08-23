2270ffafcaefea373cb2922bc3c2c4d3765f4cce
iki1uc <entertrainer@web.de>
2026-08-23 19:53:53 +0200

    Update vector.flow.js

--- vector.flow.js
+++ vector.flow.js
@@ -1,52 +1,9 @@
-// vector.flow.js
-// Potender VECTOR (steigend) & Abfall VECTOR (fallend)
+const FAIL_NEUTRAL = require("./fail.neutral.js");
 
-// QI = Reinheit des Tatbestands
-function QI(rolle, ort, home) {
-    return rolle + ort + home;
+function FLOW(input) {
+  try {
+    // dein Flow
+  } catch (error) {
+    return FAIL_NEUTRAL(error);
+  }
 }
-
-// IQQ = Reinheit des Vorgangs
-function IQQ(suite, bildung) {
-    return suite + bildung;
-}
-
-// ENGINE = Kraft, die VECTOR antreibt
-function ENGINE(qi, iqq) {
-    return qi * 0.6 + iqq * 0.4; 
-}
-
-// POTENDER VECTOR = steigende Bewegung
-function potenderVector(qi, iqq) {
-    const engine = ENGINE(qi, iqq);
-    const richtung = engine + (qi - iqq);
-
-    return {
-        typ: "potender",
-        engine,
-        richtung,
-        ausgang: engine + richtung
-    };
-}
-
-// ABFALL VECTOR = fallende Bewegung
-function abfallVector(qi, iqq) {
-    const engine = ENGINE(qi, iqq);
-    const richtung = engine - (qi + iqq);
-
-    return {
-        typ: "abfall",
-        engine,
-        richtung,
-        ausgang: engine + richtung
-    };
-}
-
-// Export
-module.exports = {
-    QI,
-    IQQ,
-    ENGINE,
-    potenderVector,
-    abfallVector
-};

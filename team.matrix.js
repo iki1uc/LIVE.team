69e209053439cefa2af8c48bceb84c632779b264
iki1uc <entertrainer@web.de>
2026-08-23 19:54:10 +0200

    Update team.matrix.js

--- team.matrix.js
+++ team.matrix.js
@@ -1,18 +1,4 @@
-const TEAM_MATRIX = {
-  achse: "ridvan",
-  rollen: {
-    technik: { aufgabe: "Rechner, CPU, RAM, GPU" },
-    web: { aufgabe: "Homepage, UI, Team-Seite" },
-    connect: { aufgabe: "Menschen finden, Gespräche" },
-    raum: { aufgabe: "Ort, Internet-Café, Setup" }
-  },
-  verbindung: {
-    technik: ["web", "raum"],
-    web: ["connect"],
-    connect: ["raum"],
-    raum: ["achse"]
-  }
-};
-
-module.exports = TEAM_MATRIX;
-
+fail: {
+  regel: "Fail wird nicht gelöscht, sondern neutralisiert.",
+  methode: "FAIL_NEUTRAL"
+}

013e5cf54083d198891ca830bd956751440831ac
iki1uc <entertrainer@web.de>
2026-08-23 19:49:17 +0200

    Update team.matrix.js

--- team.matrix.js
+++ team.matrix.js
@@ -1,1 +1,18 @@
+const TEAM_MATRIX = {
+  achse: "ridvan",
+  rollen: {
+    technik: { aufgabe: "Rechner, CPU, RAM, GPU" },
+    web: { aufgabe: "Homepage, UI, Team-Seite" },
+    connect: { aufgabe: "Menschen finden, Gespräche" },
+    raum: { aufgabe: "Ort, Internet-Café, Setup" }
+  },
+  verbindung: {
+    technik: ["web", "raum"],
+    web: ["connect"],
+    connect: ["raum"],
+    raum: ["achse"]
+  }
+};
+
+module.exports = TEAM_MATRIX;
 
